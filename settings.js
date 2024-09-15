aaf406d66e53132c5f68954266f565df3f2fa7f6
Muzybaba <muzzyothmam@gmail.com>
2024-09-15 23:39:05 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -11,9 +11,9 @@ global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'Muzybabav2' //ur bot name
+global.ownernumber = '2348123124998' //ur owner number
+global.ownername = 'muzammil' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
